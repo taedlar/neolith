995de36b33095191396dea11b6a67f3b2f0f05dd
Annihilator <taedlar@gmail.com>
2022-09-30 23:30:48 +0800

    use variadic macro for debug logging

--- src/logger.h
+++ src/logger.h
@@ -6,9 +6,15 @@ extern int log_message (const char* file, const char *fmt, ...);
 extern int debug_message (char *, ...);
 extern int debug_message_with_src (const char* func, const char* src, int line, const char* fmt, ...);
 
-#define debug_trace(what)		debug_message_with_src(__func__, __FILE__, __LINE__, "%s", (what))
+#if __STDC_VERSION__ >= 199901L
+#define debug_fatal(...)		debug_message_with_src(__func__, __FILE__, __LINE__, __VA_ARGS__)
+#define debug_error(...)		debug_message_with_src(__func__, __FILE__, __LINE__, __VA_ARGS__)
+#define debug_warn(...)			debug_message_with_src(__func__, __FILE__, __LINE__, __VA_ARGS__)
+#define debug_info(...)			debug_message_with_src(__func__, __FILE__, __LINE__, __VA_ARGS__)
+#define debug_trace(...)		debug_message_with_src(__func__, __FILE__, __LINE__, __VA_ARGS__)
+#endif
 
-/* error loggers */
+/* specific error loggers */
 extern int debug_perror_with_src (const char* func, const char* src, int line, const char* what, const char* file);
 #define debug_perror(what,file)		debug_perror_with_src(__func__, __FILE__, __LINE__, (what), (file))
 
