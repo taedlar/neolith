0a1e52b42870c9479d2946a27186f2e61fd85b88
taedlar <taedlar@gmail.com>
2025-10-21 00:25:40 +0800

    test: add stem object library for unit-testing

--- src/malloc.c
+++ src/malloc.c
@@ -2,9 +2,34 @@
 #include <config.h>
 #endif /* HAVE_CONFIG_H */
 
-#define IN_MALLOC_WRAPPER
 #define NO_OPCODES
 #include "std.h"
+
+char *
+xalloc (int size)
+{
+  char *p;
+  static int going_to_exit = 0;
+
+  if (going_to_exit)
+    exit (3);
+  p = (char *) DMALLOC (size, TAG_MISC, "main.c: xalloc");
+  if (p == 0)
+    {
+      if (reserved_area)
+        {
+          FREE (reserved_area);
+          /* after freeing reserved area, we are supposed to be able to write log messages */
+          debug_message ("{}\t***** temporarily out of MEMORY. Freeing reserve.");
+          reserved_area = 0;
+          slow_shut_down_to_do = 6;
+          return xalloc (size);	/* Try again */
+        }
+      going_to_exit = 1;
+      fatal ("Totally out of MEMORY.\n");
+    }
+  return p;
+}
 
 #ifdef DO_MSTATS
 void
---
+++ tests/test_lpc_compiler/test_lpc_compiler.cpp
@@ -0,0 +1,15 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+#include <gtest/gtest.h>
+
+extern "C" {
+    #include "std.h"
+}
+
+using namespace testing;
+
+TEST(lpc_compiler, init)
+{
+
+}
