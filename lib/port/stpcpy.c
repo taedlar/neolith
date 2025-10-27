2804d419ce5d5f0eaed7306543d60ff1d7a9f63c
taedlar <taedlar@gmail.com>
2025-10-27 17:01:29 +0800

    port: detects posix stpcpy and sptncpy

--- lib/port/stpcpy.c
+++ lib/port/stpcpy.c
@@ -1,13 +1,23 @@
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif /* HAVE_CONFIG_H */
+#include "wrapper.h"
 
 #ifndef	HAVE_STPCPY
-char *
-stpcpy (char *dest, const char *src)
-{
+char* stpcpy (char *dest, const char *src) {
   while (*src)
     *dest++ = *src++;
   return dest;
 }
 #endif /* ! HAVE_STPCPY */
+
+#ifndef	HAVE_STPNCPY
+char* stpncpy (char *dest, const char *src, size_t n) {
+  size_t i;
+  for (i = 0; i < n && src[i]; i++)
+    dest[i] = src[i];
+  if (i < n)
+    dest[i] = '\0'; /* POSIX-1.2008 requires exactly n bytes copied, but we don't rely on it */
+  return dest + i;
+}
+#endif	/* ! HAVE_STPNCPY */
--- lib/port/wrapper.h
+++ lib/port/wrapper.h
@@ -10,8 +10,12 @@ char* xstrdup(const char* s);
 void* xcalloc(size_t nmemb, size_t size);
 
 #ifndef	HAVE_STPCPY
-char* stpcpy (char* dest, const char* src);
+char* stpcpy (char* dest, const char* src);     /* POSIX-1.2008, <string.h> */
 #endif	/* ! HAVE_STPCPY */
+
+#ifndef	HAVE_STPNCPY
+char* stpncpy (char* dest, const char* src, size_t n);     /* POSIX-1.2008, <string.h> */
+#endif	/* ! HAVE_STPNCPY */
 
 #ifdef __cplusplus
 }
