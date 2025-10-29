3f5056cf799c6fb3e0d38849f64c9273ad56617c
Taedlar <taedlar@gmail.com>
2025-10-29 19:07:00 +0800

    fix: save_binary signature to use simil_efun as config id

--- lib/lpc/program/binaries.h
+++ lib/lpc/program/binaries.h
@@ -1,9 +1,9 @@
 #pragma once
-#include <stdio.h>
 #include "lpc/compiler.h"
 
-FILE *crdir_fopen(char *);
 void init_binaries();
+
 #define load_binary(x, y) int_load_binary(x)
 program_t *int_load_binary(char *);
+
 void save_binary(program_t *, mem_block_t *, mem_block_t *);
--- tests/test_lpc_compiler/test_lpc_compiler.cpp
+++ tests/test_lpc_compiler/test_lpc_compiler.cpp
@@ -1,76 +1,10 @@
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif /* HAVE_CONFIG_H */
-#include <gtest/gtest.h>
-#include <filesystem>
 
-extern "C" {
-    #include "std.h"
-    #include "rc.h"
-    #include "src/simul_efun.h"
-    #include "src/uids.h"
-    #include "lpc/object.h"
-    #include "lpc/otable.h"
-}
+#include "fixtures.hpp"
 
 using namespace testing;
-
-// according to GoogleTest FAQ, the test suite name and test name should not
-// contain underscores to avoid issues on some platforms.
-// https://google.github.io/googletest/faq.html#why-should-test-suite-names-and-test-names-not-contain-underscore
-
-class LPCCompilerTest: public Test {
-private:
-    std::filesystem::path previous_cwd;
-
-protected:
-    /*  LPCCompilerTest::SetUp()
-     *  --------------------------------
-     *  Initialize the LPC compiler environment for testing.
-     *  This includes setting up logging, locale, configuration,
-     *  string management, UID management, object management,
-     *  and object table.
-     *  Also initializes identifiers, local variable management,
-     *  include paths, instruction table, stack machine,
-     *  and predefines.
-     * 
-     *  The master object and simul_efun object are NOT loaded here.
-     *  --------------------------------
-     */
-    void SetUp() override {
-        debug_set_log_with_date (0);
-        setlocale(LC_ALL, "C.UTF-8"); // force UTF-8 locale for consistent string handling
-        init_stem(3, (unsigned long)-1, "m3.conf"); // use highest debug level and enable all trace logs
-
-        init_config(SERVER_OPTION(config_file));
-
-        debug_message("[ SETUP    ] CTEST_FULL_OUTPUT");
-        ASSERT_TRUE(CONFIG_STR(__MUD_LIB_DIR__));
-        namespace fs = std::filesystem;
-        auto mudlib_path = fs::path(CONFIG_STR(__MUD_LIB_DIR__)); // absolute or relative to cwd
-        if (mudlib_path.is_relative()) {
-            mudlib_path = fs::current_path() / mudlib_path;
-        }
-        ASSERT_TRUE(fs::exists(mudlib_path)) << "Mudlib directory does not exist: " << mudlib_path;
-        previous_cwd = fs::current_path();
-        fs::current_path(mudlib_path); // change working directory to mudlib
-
-        init_strings (8192, 1000000); // LPC compiler needs this since prolog()
-        init_lpc_compiler(CONFIG_INT (__MAX_LOCAL_VARIABLES__));
-        set_inc_list (CONFIG_STR (__INCLUDE_DIRS__)); // automatically freed in deinit_lpc_compiler()
-
-        eval_cost = CONFIG_INT (__MAX_EVAL_COST__); /* simulates calling LPC code from backend */
-    }
-
-    void TearDown() override {
-        deinit_lpc_compiler();
-        deinit_strings();
-
-        namespace fs = std::filesystem;
-        fs::current_path(previous_cwd);
-        deinit_config();
-    }
-};
 
 TEST_F(LPCCompilerTest, compileFile) {
     error_context_t econ;
