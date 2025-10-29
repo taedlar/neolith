5c8c11893935ad50d9ad5d253c49827a8abdbb29
Ted Zhang <taedlar@gmail.com>
2025-10-29 22:19:12 +0800

    test: unit-test calling simul_efun (#118)

--- tests/test_simul_efuns/test_simul_efuns.cpp
+++ tests/test_simul_efuns/test_simul_efuns.cpp
@@ -142,3 +142,44 @@ TEST_F(SimulEfunsTest, findSimulEfun)
     }
     pop_context (&econ);
 }
+
+TEST_F(SimulEfunsTest, callSimulEfun)
+{
+    error_context_t econ;
+    save_context (&econ);
+    if (setjmp(econ.context)) {
+        restore_context (&econ);
+        // FAIL() << "Failed to load simul efuns.";
+    }
+    else {
+        ASSERT_EQ(get_machine_state(), MS_PRE_MUDLIB);
+        init_simul_efun ("/simul_efun_test.c");
+        ASSERT_TRUE(simul_efun_ob != nullptr) << "simul_efun_ob is null after init_simul_efun().";
+        // simul_efun_ob should have ref count 2: one from set_simul_efun, one from get_empty_object
+        EXPECT_EQ(simul_efun_ob->ref, 2) << "simul_efun_ob reference count is not 2 after init_simul_efun().";
+
+        char* func_name = findstring("sum");
+        ASSERT_TRUE(func_name != nullptr) << "Failed to find string 'sum'.";
+        int index = find_simul_efun(func_name);
+
+        // call sum(10, 20) via simul efun
+        current_object = simul_efun_ob;
+        push_number(10);
+        push_number(20);
+        call_simul_efun((unsigned short)index, 2);
+        EXPECT_TRUE(sp->type == T_NUMBER) << "Return value type from simul efun 'sum' is not T_NUMBER.";
+        EXPECT_EQ(sp->u.number, 30) << "Return value from simul efun 'sum' is not correct.";
+        push_constant_string ("0");
+
+        // call concat(30, "0") via simul efun
+        func_name = findstring("concat");
+        ASSERT_TRUE(func_name != nullptr) << "Failed to find string 'concat'.";
+        index = find_simul_efun(func_name);
+        call_simul_efun((unsigned short)index, 2);
+        EXPECT_TRUE(sp->type == T_STRING) << "Return value type from simul efun 'concat' is not T_STRING.";
+        EXPECT_STREQ(sp->u.string, "300") << "Return value from simul efun 'concat' is not correct.";
+
+        pop_stack(); // pop string result
+    }
+    pop_context (&econ);
+}
