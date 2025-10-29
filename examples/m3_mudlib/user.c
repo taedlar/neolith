6fd858b0b4bc31ef72b8254afcadafe716fe1d54
taedlar <taedlar@gmail.com>
2025-10-30 01:27:42 +0800

    test: add LPC lexer unit-tests

--- examples/m3_mudlib/user.c
+++ examples/m3_mudlib/user.c
@@ -1,14 +1,14 @@
 // vim: syntax=lpc
-private void
-logon()
+#include "m3_config.h"
+
+private void logon()
 {
   write("Welcome to the M3 Mud!\n");
   enable_commands();
   add_action("cmd_quit", "quit");
 }
 
-private int
-cmd_quit (string arg)
+private int cmd_quit (string arg)
 {
   write("Bye!\n");
   destruct(this_object());
