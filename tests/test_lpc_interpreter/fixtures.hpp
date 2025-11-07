037500e063f0147f74e9514507c2446d96e7d21d
taedlar <taedlar@gmail.com>
2025-11-07 23:43:11 +0800

    test: add unit-tests for lpc interpreter

---
+++ tests/test_lpc_interpreter/fixtures.hpp
@@ -0,0 +1,64 @@
+#pragma once
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif /* HAVE_CONFIG_H */
+
+#include <gtest/gtest.h>
+#include <filesystem>
+
+extern "C" {
+    #include "std.h"
+    #include "rc.h"
+    #include "src/simul_efun.h"
+    #include "uids.h"
+    #include "lpc/object.h"
+    #include "lpc/otable.h"
+}
+
+using namespace testing;
+
+// according to GoogleTest FAQ, the test suite name and test name should not
+// contain underscores to avoid issues on some platforms.
+// https://google.github.io/googletest/faq.html#why-should-test-suite-names-and-test-names-not-contain-underscore
+
+class LPCInterpreterTest: public Test {
+private:
+    std::filesystem::path previous_cwd;
+
+protected:
+    void SetUp() override {
+        debug_set_log_with_date (0);
+        setlocale(LC_ALL, "C.UTF-8"); // force UTF-8 locale for consistent string handling
+        init_stem(3, (unsigned long)-1, "m3.conf"); // use highest debug level and enable all trace logs
+
+        init_config(MAIN_OPTION(config_file));
+
+        debug_message("[ SETUP    ] CTEST_FULL_OUTPUT");
+        ASSERT_TRUE(CONFIG_STR(__MUD_LIB_DIR__));
+        namespace fs = std::filesystem;
+        auto mudlib_path = fs::path(CONFIG_STR(__MUD_LIB_DIR__)); // absolute or relative to cwd
+        if (mudlib_path.is_relative()) {
+            mudlib_path = fs::current_path() / mudlib_path;
+        }
+        ASSERT_TRUE(fs::exists(mudlib_path)) << "Mudlib directory does not exist: " << mudlib_path;
+        previous_cwd = fs::current_path();
+        fs::current_path(mudlib_path); // change working directory to mudlib
+
+        init_strings (8192, 1000000); // LPC compiler needs this since prolog()
+        init_lpc_compiler(CONFIG_INT (__MAX_LOCAL_VARIABLES__));
+        set_inc_list (CONFIG_STR (__INCLUDE_DIRS__)); // automatically freed in deinit_lpc_compiler()
+
+        init_simulate();
+        eval_cost = CONFIG_INT (__MAX_EVAL_COST__); /* simulates calling LPC code from backend */
+    }
+
+    void TearDown() override {
+        tear_down_simulate();
+        deinit_lpc_compiler();
+        deinit_strings();
+
+        namespace fs = std::filesystem;
+        fs::current_path(previous_cwd);
+        deinit_config();
+    }
+};
---
+++ tests/test_lpc_interpreter/test_lpc_interpreter.cpp
@@ -0,0 +1,35 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include "fixtures.hpp"
+extern "C" {
+    #include "lpc/program.h"
+    #include "lpc/program/disassemble.h"
+}
+
+TEST_F(LPCInterpreterTest, disassemble) {
+    error_context_t econ;
+    save_context (&econ);
+    if (setjmp(econ.context)) {
+        restore_context (&econ);
+        FAIL() << "Failed to compile master.c.";
+    }
+    else {
+        // compile a simple test file
+        int fd = open("master.c", O_RDONLY);
+        ASSERT_NE(fd, -1) << "Failed to open master.c for reading.";
+        program_t* prog = compile_file(fd, "master.c");
+        ASSERT_TRUE(prog != nullptr) << "compile_file returned null program.";
+        total_lines = 0;
+        close(fd);
+
+        // compiler_function_t* funp = prog->function_table; // index 0
+        // fprintf(stderr, "function#0 name = '%s'\n", funp->name);
+        EXPECT_NO_THROW(disassemble (stderr, prog->program, 0, prog->program_size, prog));
+
+        // free the compiled program
+        free_prog(prog, 1);
+    }
+    pop_context (&econ);
+}
